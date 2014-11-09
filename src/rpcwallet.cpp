271a5c09f77c71640424abf7b04488b22f28eac0
YarkoL <yarko.dev@gmail.com>
2014-11-09 20:36:27 +0200

    advertised balance shows in getinfo

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -71,6 +71,12 @@ string AccountFromValue(const Value& value)
     return strAccount;
 }
 
+int calculateAdvertisedBalanceCoins(int nAdvBal) {
+    double fraction = nAdvBal/100.0;
+    double intermediate =  fraction * pwalletMain->GetBalance();
+    return (int)(intermediate / COIN);
+}
+
 Value getinfo(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 0)
@@ -86,6 +92,7 @@ Value getinfo(const Array& params, bool fHelp)
     obj.push_back(Pair("protocolversion",(int)PROTOCOL_VERSION));
     obj.push_back(Pair("walletversion", pwalletMain->GetVersion()));
     obj.push_back(Pair("balance",       ValueFromAmount(pwalletMain->GetBalance())));
+    obj.push_back(Pair("advertisedbalance",calculateAdvertisedBalanceCoins(nAdvertisedBalance)));
     obj.push_back(Pair("newmint",       ValueFromAmount(pwalletMain->GetNewMint())));
     obj.push_back(Pair("stake",         ValueFromAmount(pwalletMain->GetStake())));
     obj.push_back(Pair("blocks",        (int)nBestHeight));
