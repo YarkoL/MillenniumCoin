4e3ae6867332afefb80ccb5587a93f85e37392e7
YarkoL <yarko.dev@gmail.com>
2015-01-05 21:48:39 +0200

    incremented version revision

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ set<pair<COutPoint, unsigned int> > setStakeSeen;
 
 CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // "standard" scrypt target limit for proof of work, results with 0,000244140625 proof-of-work difficulty
 CBigNum bnProofOfStakeLimit(~uint256(0) >> 20);
-CBigNum bnProofOfWorkLimitTestNet(~uint256(0) >> 16);
+CBigNum bnProofOfWorkLimitTestNet(~uint256(0) >> 10);
 
 unsigned int nTargetSpacing = 1 * 30; // 60 seconds
 unsigned int nStakeMinAge = 3 * 24 * 60 * 60; // 3 days
